6fe676fcec4623e5f20a1f0da50d51e8773fe65f
d3k4 c4s3 <d3k4_k3rb3r0s@proton.me>
2024-05-28 14:53:03 -0600

    Update d3ckn3t_s3rv3.sh

--- d3ckn3t_s3rv3.sh
+++ d3ckn3t_s3rv3.sh
@@ -23,10 +23,12 @@ usage() {
                   | |  |___/____/|_|    \_/ |____/   | |
                   |__|                              |__|
 EOF
-    echo "Usage: d3ckn3t-s3rv3 [-p <port>] [-d <directory>]"
-    echo "Options:"
-    echo "  -p, --port <port>       Specify the port number (default is $default_port)"
-    echo "  -d, --directory <path>  Specify the directory to serve files from"
+    echo -e "\nUsage: d3ckn3t-s3rv3 [-p <port>] [-d <directory>]"
+    echo -e "\nOptions:"
+    echo "  -p, --port <port>       Specify the port number. (Default: $default_port)"
+    echo "  -d, --directory <path>  Specify the directory to serve files from."
+    echo
+    echo "  -h, --help              Display this help message.
     exit 1
 }
 
@@ -34,22 +36,20 @@ EOF
 cleanup() {
         clear
         #cat <<"EOF"
-#[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]
-#
 #         __       _ _____      _                    _____ _     __ 
 #        | _|   __| |___ /  ___| | __          _ __ |___ /| |_  |_ |
 #        | |   / _` | |_ \ / __| |/ /  _____  | '_ \  |_ \| __|  | |
 #        | |  | (_| |___) | (__|   <  |_____| | | | |___) | |_   | |
 #        | |   \__,_|____/ \___|_|\_\         |_| |_|____/ \__|  | |
 #        |__|                                                   |__|
-#                           [d3k@t3dd3r4]:[2024]
+#                           [d3ckn3t-s3rv3]:[v1]
 #                   __       _____            _____   __ 
 #                  | _|  ___|___ / _ ____   _|___ /  |_ |
 #                  | |  / __| |_ \| '__\ \ / / |_ \   | |
 #                  | |  \__ \___) | |   \ V / ___) |  | |
 #                  | |  |___/____/|_|    \_/ |____/   | |
 #                  |__|                              |__|
-#[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓][↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]
+#                           [d3k@t3ss3r4]:[2024]
 #EOF
 
 #laptop ascii
@@ -66,24 +66,24 @@ cleanup() {
 
 |[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]|
 EOF
-    sleep .5
+    sleep .3
     echo
     echo -e "[\e[5m!\e[25m]:[d3ckn3t-s3rv3]:[operator.interference]"
-    sleep .3
+    sleep .2
     echo -e "[\e[5m!\e[25m]:[\e[5mABORT\e[25m]:[\e[5m!\e[25m]:[d3ckn3t-s3rv3]:[\e[5mABORT\e[25m]:[\e[5m!\e[25m]\n"
     sleep 1
     echo "[x]:[d3ckn3t-s3rv3]:[deactivating server]:[-]"
-    sleep .3
+    sleep .2
     echo "[.]:[...]"
-    sleep .3
+    sleep .2
     echo -e "[\e[5m↓\e[25m]:[d3ckn3t-s3rv3]:[\e[5m↓\e[25m]:[$hostname]:[$directory]:[server]:[$ip_address]:[$port]:[\e[5m↓\e[25m]:[-]"
-    sleep .3
+    sleep .2
     echo -e "[.]:[...]"
-    sleep .3
+    sleep .2
     echo -e "[\e[5mx\e[25m]:[d3ckn3t-s3rv3]:[exiting]:[...]:[\e[5mx\e[25m]:\n"
-    sleep 1
+    sleep .5
     echo -e "[.]:[...]"
-    sleep 2
+    sleep 1.5
     clear
 
 
@@ -122,13 +122,48 @@ if [ -z "$directory" ]; then
     directory=$(pwd)
     fi
 
-# Check if the provided directory exists
-if [ ! -d "$directory" ]; then
-    echo "[x]:[$hostname]:[server]:[invalid input]:[Directory $directory does not exist.]"
+while [ ! -d "$directory" ]; do
+sleep .5
+clear
+#cat <<"EOF"
+#         __       _ _____      _                    _____ _     __ 
+#        | _|   __| |___ /  ___| | __          _ __ |___ /| |_  |_ |
+#        | |   / _` | |_ \ / __| |/ /  _____  | '_ \  |_ \| __|  | |
+#        | |  | (_| |___) | (__|   <  |_____| | | | |___) | |_   | |
+#        | |   \__,_|____/ \___|_|\_\         |_| |_|____/ \__|  | |
+#        |__|                                                   |__|
+#                           [d3ckn3t-s3rv3]:[v1]
+#                   __       _____            _____   __ 
+#                  | _|  ___|___ / _ ____   _|___ /  |_ |
+#                  | |  / __| |_ \| '__\ \ / / |_ \   | |
+#                  | |  \__ \___) | |   \ V / ___) |  | |
+#                  | |  |___/____/|_|    \_/ |____/   | |
+#                  |__|                              |__|
+#                           [d3k@t3ss3r4]:[2024]
+#EOF
+
+#laptop ascii
+    cat <<"EOF"
+|[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]|
+
+     __       _ _____      _           _____ _                 _____            _____   __    
+    | _|   __| |___ /  ___| | __ _ __ |___ /| |_           ___|___ / _ ____   _|___ /  |_ |     [d3ckn3t-s3rv3]:[v1]
+    | |   / _` | |_ \ / __| |/ /| '_ \  |_ \| __|  _____  / __| |_ \| '__\ \ / / |_ \   | |     [d3k@t3ss3r4]:[2024]
+    | |  | (_| |___) | (__|   < | | | |___) | |_  |_____| \__  \__) | |   \ V / ___) |  | |     [hack.the.planet]
+    | |   \__,_|____/ \___|_|\_\|_| |_|____/ \__|         |___/____/|_|    \_/ |____/   | |     [all.your.nekworkz.are.belong.us]
+    |__|                                                                               |__|
+
+
+|[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]:[↓]|
+EOF
     sleep .5
-    echo "[.]:[...]"
-    exit 1
-fi
+    echo
+    echo -e "\n[x]:[$hostname]:[server]:[invalid input]:[Directory $directory does not exist.]"
+    sleep .5
+    echo -e "[.]:[...]\n"
+    echo -n "Please enter a valid directory path: "
+    read directory
+done
 
 # Change to the provided directory
 cd "$directory" || exit
@@ -136,10 +171,8 @@ cd "$directory" || exit
 # Start the Python 3 HTTP server
 clear
 echo
-#cyberdeck ascii
     #cat <<"EOF"
-#[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]:[↑]|
-#         __       _ _____      _                    _____ _     __
+#         __       _ _____      _                    _____ _     __ 
 #        | _|   __| |___ /  ___| | __          _ __ |___ /| |_  |_ |
 #        | |   / _` | |_ \ / __| |/ /  _____  | '_ \  |_ \| __|  | |
 #        | |  | (_| |___) | (__|   <  |_____| | | | |___) | |_   | |
@@ -174,30 +207,31 @@ echo
 echo "[=]:[d3ckn3t-s3rv3]:[configuration]:[initializing]:[>]"
 sleep .3
 echo "[.]:[...]"
-sleep .3
+sleep .1
 echo "[=]:[d3ckn3t-s3rv3]:[configuring]:[hostname]:[$hostname]:[+]"
 sleep .3
 echo "[.]:[...]"
-sleep .3
+sleep .1
 echo "[=]:[d3ckn3t-s3rv3]:[configuring]:[ipv4]:[$ip_address]:[+]"
 sleep .3
 echo "[.]:[...]"
-sleep .3
+sleep .1
 echo "[=]:[d3ckn3t-s3rv3]:[configuring]:[directory]:[$directory]:[+]"
 sleep .3
 echo "[.]:[...]"
-sleep .3
+sleep .1
 echo "[=]:[d3ckn3t-s3rv3]:[configuring]:[port]:[$port]:[+]"
 sleep .3
 echo "[.]:[...]"
-sleep .3
+sleep .1
 echo "[.]:[d3ckn3t-s3rv3]:[deploying server]:[\e[5m↑\e[25m]"
 sleep .3
 echo "[.]:[...]"
-sleep .3
+sleep .1
 echo -e "[\e[5m↑\e[25m]:[d3ckn3t-s3rv3]:[\e[5m↑\e[25m]:[$hostname]:[$directory]:[server]:[$ip_address]:[$port]:[\e[5m↑\e[25m]"
 sleep .3
 echo "[.]:[...]"
 echo -e "[\e[5m↑\e[25m]:[d3ckn3t-s3rv3]:[x]:[_sigterm=quit_]:[x]:[\e[5m↑\e[25m]:[\e[5m↑\e[25m]:[\e[5m↑\e[25m]:[\e[5m↑\e[25m]:\n"
 
 (python3 -m http.server "$port")
+
